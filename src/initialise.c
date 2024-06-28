ef9bc42e7127a5ff688dc9bd36f651c09e49a084
tcampbel <tcampbel@Shitty-HP.rev.dnainternet.fi>
2024-06-28 17:00:59 +0300

    Removed 2 layers of mallocing, compressed init functions. Inititialing now complete

--- src/initialise.c
+++ src/initialise.c
@@ -3,84 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   initialise.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tcampbel <tcampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:55:30 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/06/27 16:30:14 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:50:44 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	init_mutex(t_data *waiter)
+static int	assign_forks(t_philo *ph, t_fork *forks, int philo_pos)
 {
-	int	i;
+	int	ph_num;
 
-	i =  -1;
-	waiter->fork = malloc(sizeof(t_fork));
-	if (!waiter->fork)
-		return (ft_perror("malloc failure\n"));
-	waiter->fork->forks= malloc(sizeof(pthread_mutex_t) * (waiter->ph_num + 1));
-	if (!waiter->fork->forks)
-		return (ft_perror("malloc failure\n"));
-	while (++i < waiter->ph_num)
-		pthread_mutex_init(&waiter->fork->forks[i], NULL);
-	return (0);
-
-}
-int	init_data_struct(t_data *waiter, char **av)
-{	
-	waiter->ph_num = ft_atol(av[1]);
-	if (waiter->ph_num > 1000)
-		ft_perror("Not enough seats at the table :(\n");
-	waiter->time_to_die = ft_atol(av[2]);
-	waiter->time_to_eat = ft_atol(av[3]);
-	waiter->time_to_sleep = ft_atol(av[4]);
-	if (av[5])
-		waiter->meal_count = ft_atol(av[5]);
-	else
-		waiter->meal_count = -1;
-	if (init_philo_struct(av, waiter->ph, waiter->ph_num))
-		return (1);
-	if (init_mutex(waiter))
-		return (1);
-	return (0);
+	ph_num = ph->table->ph_num;
+	ph->left_fork = &forks[(philo_pos + 1) % ph_num];
+	ph->right_fork = &forks[philo_pos];
+	if (philo_pos % 2 == 0)
+	{
+		ph->right_fork = &forks[philo_pos];
+		ph->left_fork = &forks[(philo_pos + 1) % ph_num];
+	}
+	printf("philo[%zu]: Has fork[%zu] in left hand and fork[%zu] in right hand\n", ph->id, ph->left_fork->fork_id, ph->right_fork->fork_id);
+	return (EXIT_SUCCESS);
 }
 
-int	init_philo_struct(char **av, t_philo *ph, long ph_num)
+static int	init_philo_struct(t_table *table, t_philo *ph, long ph_num)
 {
 	int	i;
 
 	i = -1;
-	ph = malloc(sizeof(t_philo) * (ph_num + 1));
-	if (!ph)
-		return(ft_perror("malloc failure"));
-	while (++i < ft_atol(av[1]))
+	while (++i < ph_num)
 	{
 		ph[i].id = i + 1;
-		ph->thread = create_thread(ph->thread);
-		if (ph->thread == 1)
-			return (1);	
-		//printf("ph_num = %ld\ntime_to_die = %ld\ntime_to_eat = %ld\ntime_to_sleep = %ld\nx_eat = %ld\n", ph[i].ph_num, ph[i].time_to_die, ph[i].time_to_eat, ph[i].time_to_sleep, ph[i].x_eat);
+		ph[i].full = false;
+		ph[i].meals_eaten = 0;
+		ph[i].table = table;
+		assign_forks(&ph[i], table->forks, i);
 	}
-	return (0);
-}
-void	*routine()
-{
-	int	test;
-
-	test = 0;
-	while (test < 20)
-		test++;
-	printf("%d\n", test);
-	return (NULL);
+	return (EXIT_SUCCESS);
 }
 
-pthread_t	create_thread(pthread_t thread)
+static int	init_mutex(t_fork *forks, long ph_num)
 {
-	if (pthread_create(&thread, NULL, &routine, NULL))
-		return(ft_perror("failed to create thread\n"));
-	if (pthread_join(thread, NULL))
-		return(ft_perror("thread failed to join"));
-	return (thread);
+	int	i;
+
+	i =  -1;
+	while (++i < ph_num)
+	{
+		if (pthread_mutex_init(&forks[i].fork, NULL))
+			return (ft_perror("mutex failed to initailise\n"));
+		forks[i].fork_id = i;
+	}
+	return (EXIT_SUCCESS);
+
+}
+static int	init_table(t_table *table, char **av)
+{
+	table->ph = NULL;
+	table->forks = NULL;
+	table->end_dinner = false;
+	table->ph_num = ft_atol(av[1]);
+	if (table->ph_num > 1000)
+		ft_perror("Not enough seats at the table :(\n");
+	table->time_to_die = ft_atol(av[2]) * 1000;
+	table->time_to_eat = ft_atol(av[3]) * 1000;
+	table->time_to_sleep = ft_atol(av[4]) * 1000;
+	if (av[5])
+		table->meal_count = ft_atol(av[5]);
+	else
+		table->meal_count = -1;
+	table->ph = malloc(sizeof(t_philo) * table->ph_num);
+	table->forks = malloc(sizeof(t_fork) * table->ph_num);
+	if (!table->ph || !table->forks)
+		return(ft_perror("malloc failure"));
+	return (EXIT_SUCCESS);
+}
+
+int	init_structs(t_table *table, char **av)
+{	
+	if (init_table(table, av))
+		return (EXIT_FAILURE);
+	if (init_mutex(table->forks, table->ph_num))
+		return (EXIT_FAILURE);
+	if (init_philo_struct(table, table->ph, table->ph_num))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
