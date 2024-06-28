3bf01012f858fdc7880118a8633db3a5edad1cc6
tcampbel <tcampbel@Shitty-HP.rev.dnainternet.fi>
2024-06-28 16:57:53 +0300

    Added exit e_nums and removed unecessary prototypes

--- inc/philo.h
+++ inc/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tcampbel <tcampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:19:57 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/06/27 16:28:35 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/06/28 16:43:32 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,58 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/time.h>
 
 # define INT_MAX 2147483647
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
 
-typedef struct s_data t_data;
+typedef pthread_mutex_t t_mutex;
+typedef struct s_table t_table;
+
+typedef enum e_bool
+{
+	true = 1,
+	false = 0,
+}	t_bool;
 
 typedef struct s_fork
 {
-	pthread_mutex_t	*forks;	
-	size_t			fork_id;
+	t_mutex	fork;	
+	size_t	fork_id;
 }	t_fork;
 
 typedef struct s_philo
 {
 	pthread_t	thread;
 	size_t		id;
-	t_data		*waiter;
+	t_bool		full;
+	size_t		meals_eaten;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	t_table		*table;	
 }	t_philo;
 
-typedef struct s_data
+typedef struct s_table
 {
-	t_philo	*ph;
-	t_fork	*fork;
-	long	ph_num;
-	size_t	time_to_die;
-	size_t	time_to_eat;
-	size_t	time_to_sleep;
-	int		meal_count;
-}	t_data;
+	t_philo		*ph;
+	t_fork		*forks;
+	long		ph_num;
+	size_t		start_time;
+	size_t		time_to_die;
+	size_t		time_to_eat;
+	size_t		time_to_sleep;
+	int			meal_count;
+	t_bool		end_dinner;
+	pthread_t	waiter;
+}	t_table;
 
 long		ft_atol(const char *str);
-int			ft_is_num(char c);
 int			check_args(char **av);
-pthread_t	create_thread(pthread_t thread);
-int			init_philo_struct(char **av, t_philo *ph, long ph_num);
-int			init_mutex(t_data  *waiter);
-int			init_data_struct(t_data  *waiter, char **av);
-pthread_t	create_thread(pthread_t thread);
+int			init_structs(t_table *table, char **av);
+int			start_dinner(t_table *table);
 int			ft_perror(char *str);
+size_t		get_time(t_table *table);
 
 
 #endif
