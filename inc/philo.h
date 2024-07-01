f9954b485dd336d67d7e088a094f6e8824a272f0
tcampbel <tcampbel@Shitty-HP.rev.dnainternet.fi>
2024-07-01 17:33:52 +0300

    added new definitions and routine e_nums. Changed size_t to long for consistency

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:19:57 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/06/28 16:43:32 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/07/01 16:25:14 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,31 @@
 # define INT_MAX 2147483647
 # define EXIT_SUCCESS 0
 # define EXIT_FAILURE 1
+# define MTX_LOCK_ERR "mutex failed to lock\n"
+# define MTX_ULOCK_ERR "mutex failed to unlock\n"
+# define MTX_INIT_ERR "mutex failed to initialise\n"
+# define MTX_DESTROY_ERR "mutex failed to destory\n"
+# define PTHREAD_CR_ERR "failed to create thread\n"
+# define PTHREAD_JOIN_ERR "failed to join thread\n"
+# define TIME_ERR "failed to get time\n"
+# define ARG_ERR "invalid arg\n"
+# define ARG_SIZE_ERR "int max exceeded\n"
+# define ARG_COUNT_ERR "invalid arg amount\n"
+# define MALLOC_ERR "failed to allocate memory\n"
+# define MEAL_COUNT_ERR "meal count is zero, boooooo!\n"
 
 typedef pthread_mutex_t t_mutex;
 typedef struct s_table t_table;
+
+typedef enum e_ph_status
+{
+	L_FORK,
+	R_FORK,
+	EATING,
+	SLEEPING,
+	THINKING,
+	DEAD,
+}	t_ph_status;
 
 typedef enum e_bool
 {
@@ -35,40 +57,48 @@ typedef enum e_bool
 typedef struct s_fork
 {
 	t_mutex	fork;	
-	size_t	fork_id;
+	long	fork_id;
 }	t_fork;
 
 typedef struct s_philo
 {
 	pthread_t	thread;
-	size_t		id;
+	long		id;
+	long		meals_eaten;
+	long		last_meal_time;
 	t_bool		full;
-	size_t		meals_eaten;
 	t_fork		*left_fork;
 	t_fork		*right_fork;
+	t_mutex		full_lock;
+	t_mutex		meals_eaten_lock;
+	t_mutex		last_meal_time_lock;
 	t_table		*table;	
 }	t_philo;
 
 typedef struct s_table
 {
+	long		ph_num;
+	long		start_time;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		meal_count;
+	t_bool		end_dinner;
+	t_mutex		dead_lock;
 	t_philo		*ph;
 	t_fork		*forks;
-	long		ph_num;
-	size_t		start_time;
-	size_t		time_to_die;
-	size_t		time_to_eat;
-	size_t		time_to_sleep;
-	int			meal_count;
-	t_bool		end_dinner;
 	pthread_t	waiter;
 }	t_table;
 
 long		ft_atol(const char *str);
+int			ft_strlen(char *str);
 int			check_args(char **av);
 int			init_structs(t_table *table, char **av);
 int			start_dinner(t_table *table);
 int			ft_perror(char *str);
-size_t		get_time(t_table *table);
+long		get_time(void);
+void		precise_usleep(long time);
+int			print_status(t_philo *ph, t_ph_status status);
 
 
 #endif
